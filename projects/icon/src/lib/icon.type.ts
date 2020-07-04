cb3babad100e324813cd9472709ea4ca7b171a4e
Joster422 <Joster422@gmail.com>
2020-07-04 13:51:40 -0400

    chore - gh-pages deploy

--- projects/icon/src/lib/icon.type.ts
+++
@@ -1,3 +0,0 @@
-import { iconTypes } from './icon-types.const';
-
-export type icon = (typeof iconTypes)[number];
--- projects/icon/src/public-api.ts
+++
@@ -1,4 +0,0 @@
-// Public API Surface of icon
-export * from './lib/icon.component';
-export * from './lib/icon.module';
-export * from './lib/icon-types.const';
