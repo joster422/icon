0a990069e91e7f1137f76426083cd4209a26f48b
Joster422 <Joster422@gmail.com>
2020-07-23 22:46:49 -0400

    use ghp for res

--- projects/playground/src/app/app.module.ts
+++ projects/playground/src/app/app.module.ts
@@ -2,10 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
-import { FormControlModule } from '@joster/form-control';
-// import { IconModule } from '@joster/icon';
-import { IconModule } from 'dist/icon';
-
+import { FormControlModule } from '@joster-dev/form-control';
+// import { IconModule } from 'dist/icon';
+import { IconModule } from '@joster-dev/icon';
 
 import { AppComponent } from './app.component';
 
