2a3f450fca846df5003fe586f9ade1ad7cf321b1
Joster422 <Joster422@gmail.com>
2021-01-22 23:25:10 -0500

    refactor

--- projects/icon/src/lib/icon-stack/icon-stack.component.spec.ts
+++ projects/icon/src/lib/icon-stack/icon-stack.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { IconStackComponent } from './icon-stack.component';
 import { DebugElement } from '@angular/core';
@@ -7,11 +7,11 @@ describe('IconStackComponent', () => {
   let component: IconStackComponent;
   let fixture: ComponentFixture<IconStackComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
       declarations: [IconStackComponent]
     }).compileComponents();
-  }));
+  });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(IconStackComponent);
