c3f9ea9f720068d05c08e7247192f9d4d9066695
Joster422 <Joster422@gmail.com>
2021-05-28 23:07:05 -0400

    app module

--- projects/demo/src/app/app.module.ts
+++ projects/demo/src/app/app.module.ts
@@ -1,5 +1,11 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+
+import { MarkdownModule } from 'ngx-markdown';
+
+import { IconModule } from 'dist/icon';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +14,11 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    IconModule,
+    HttpClientModule,
+    MarkdownModule.forRoot({ loader: HttpClient })
   ],
   providers: [],
   bootstrap: [AppComponent]
